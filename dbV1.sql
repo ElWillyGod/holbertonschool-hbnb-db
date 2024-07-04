e5556c56ba269a064593405edb26c792a9e47ad6
Alison <8661@holbertonstudents.com>
2024-07-04 16:09:13 -0300

    db corrected

--- dbV1.sql
+++ dbV1.sql
@@ -1,13 +1,9 @@
-CREATE TABLE hbnb;
-
-USE hbnb;
-
 CREATE TABLE `users` (
   `id` varchar(255) UNIQUE PRIMARY KEY NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) UNIQUE NOT NULL,
-  `firstName` varchar(255),
-  `lastName` varchar(255),
+  `first_name` varchar(255),
+  `last_name` varchar(255),
   `role` varchar(255),
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL
@@ -15,9 +11,9 @@ CREATE TABLE `users` (
 
 CREATE TABLE `review` (
   `id` varchar(255) PRIMARY KEY NOT NULL,
-  `placeId` varchar(255) NOT NULL,
-  `userId` varchar(255) NOT NULL,
-  `reating` int NOT NULL,
+  `place_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `rating` int NOT NULL,
   `comment` varchar(255),
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL
@@ -34,35 +30,35 @@ CREATE TABLE `amenity` (
 CREATE TABLE `city` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) UNIQUE,
-  `countryCode` varchar(255),
+  `country_code` varchar(255),
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL,
-  PRIMARY KEY (`id`, `countryCode`)
+  PRIMARY KEY (`id`, `country_code`)
 );
 
 CREATE TABLE `place` (
   `id` varchar(255) PRIMARY KEY NOT NULL,
-  `userId` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `numberOfRooms` int NOT NULL,
-  `numberOfBathrooms` int NOT NULL,
-  `maxGues` int NOT NULL,
-  `pricePreNigth` float NOT NULL,
+  `number_of_rooms` int NOT NULL,
+  `number_of_bathrooms` int NOT NULL,
+  `max_guest` int NOT NULL,
+  `price_per_night` float NOT NULL,
   `latitude` float NOT NULL,
   `longitude` float NOT NULL,
-  `cityId` varchar(255) NOT NULL,
+  `city_id` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL
 );
 
-ALTER TABLE `review` ADD FOREIGN KEY (`placeId`) REFERENCES `place` (`id`);
+ALTER TABLE `review` ADD FOREIGN KEY (`place_id`) REFERENCES `place` (`id`);
 
-ALTER TABLE `review` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `place` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+ALTER TABLE `place` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `place` ADD FOREIGN KEY (`cityId`) REFERENCES `city` (`id`);
+ALTER TABLE `place` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
 CREATE TABLE `amenity_place` (
   `amenity_id` varchar,
@@ -73,5 +69,4 @@ CREATE TABLE `amenity_place` (
 ALTER TABLE `amenity_place` ADD FOREIGN KEY (`amenity_id`) REFERENCES `amenity` (`id`);
 
 ALTER TABLE `amenity_place` ADD FOREIGN KEY (`place_id`) REFERENCES `place` (`id`);
-
 
